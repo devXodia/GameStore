dea5be891d2da06b4a4bbee42f45be88af15e508
devXodia <youstylez@gmail.com>
2024-02-09 17:57:57 +0100

    added edit and delete functionality

--- obj/Debug/net7.0/GameStore.Client.AssemblyInfo.cs
+++ obj/Debug/net7.0/GameStore.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3aefa3fc83465511cb23e45cc6e17b7847c76171")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22965b4b0ef90571374d42eb0676401a1ad3f1c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
